81950c632ed200e2055d4fd98375ae00f3e40d7e
dev-vishwa <isuru.vishwanath55@gmail.com>
2025-08-04 15:18:11 +0530

    Update ModalFooter component in CreateTokenModal: adjust padding and image sizes for improved layout and visual consistency.

--- app/components/Modals/CreateTokenModal/ModalFooter.tsx
+++ app/components/Modals/CreateTokenModal/ModalFooter.tsx
@@ -16,10 +16,10 @@ interface ModalFooterProps {
 
 export const ModalFooter: FC<ModalFooterProps> = ({ feeCalculation }) => {
     return (
-        <div className="bg-[#6e3e12] p-4 flex flex-col sm:flex-row items-center justify-between gap-4">
+        <div className="bg-[#6e3e12] py-6 px-4 flex flex-col sm:flex-row items-center justify-between gap-4">
             <div className="flex items-center gap-3">
-                <img src="/images/fuel-icon.png" alt="Fuel" className="w-12 h-12" />
-                <div className="text-xs text-amber-300/80">
+                <img src="/images/fuel-icon.png" alt="Fuel" className="w-18 h-18" />
+                <div className="text-md text-amber-300/80">
                     <p>
                         <span className="font-semibold text-amber-200">GWEI:</span> 2 
                         <span className="font-semibold text-amber-200 ml-2">Token Creation Fee:</span> ${feeCalculation.tokenCreationFee.toFixed(2)}
