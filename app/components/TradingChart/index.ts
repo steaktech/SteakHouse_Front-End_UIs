06748f15adc1ecd935ec87288e74deb664dc2ca6
dev-vishwa <isuru.vishwanath55@gmail.com>
2025-09-27 21:09:48 +0530

    bug fix

--- app/components/TradingChart/index.ts
+++ app/components/TradingChart/index.ts
@@ -3,7 +3,7 @@ export { DesktopSidebar } from './DesktopSidebar';
 export { MobileBottomBar } from './MobileSidebar';
 export { TradingView } from './TradingView';
 export { TradingViewWidget } from './TradingViewWidget';
-export { TradePanel } from './TradePanel';
+// export { TradePanel } from '../Widgets/TradeWidget/TradePanel'; // Moved to Widgets
 export { FullscreenChart } from './FullscreenChart';
 export * from './types';
 export * from './mockData'; 
