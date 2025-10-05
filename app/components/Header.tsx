8e0c8be9170aa249c3dd01793df8722bef0fa72e
dev-vishwa <isuru.vishwanath55@gmail.com>
2025-10-06 00:32:20 +0530

    header bug fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -35,97 +35,93 @@ export default function Header() {
   const handleLogoClick = () => {
     router.push('/');
   };
-
   return (
     <>
       {/* <header className="relative w-full h-20 bg-gradient-to-b from-[#4c2001] to-[#723203] shadow-md"> */}
-      <header className="relative w-full h-20 bg-gradient-to-b from-[#4c2001] to-transparent shadow-md">
+      <header className="relative w-full h-16 lg:h-12 bg-gradient-to-b from-[#4c2001] to-transparent shadow-md">
         {/* Header Content */}
-        <div className="relative z-10 flex items-center justify-between h-full px-2 sm:px-4 md:px-6 lg:px-12">
+        <div className="relative z-10 flex items-center justify-between h-full px-2 sm:px-4 md:px-6 lg:px-8">
           {/* Logo & Socials Section */}
           <div className="flex items-center">
             {/* Logo Container */}
-            <div 
-              className="relative w-20 h-16 sm:w-24 sm:h-16 md:w-65 md:h-16 overflow-visible cursor-pointer hover:opacity-80 transition-opacity"
-              onClick={handleLogoClick}
-            >
+            <div className="relative w-20 h-16 sm:w-24 sm:h-16 md:w-65 md:h-16 lg:w-52 lg:h-12 pointer-events-none overflow-visible">
               <Image
                 src="/images/app-logo.png"
                 alt="Logo"
                 fill
-                className="object-contain md:hidden"
+                className="object-contain pointer-events-auto md:hidden"
               />
               <Image
                 src="/images/header-logo-lg.png"
                 alt="Logo"
                 fill
-                className="object-contain hidden md:block overflow-hidden"
+                className="object-contain pointer-events-auto hidden md:block overflow-hidden"
               />
             </div>
 
             {/* Social Icons */}
             {/* MODIFIED: Icons are hidden by default, and appear as a flex container on 'sm' screens and larger */}
-            <div className="hidden sm:flex items-center space-x-2 ml-4">
+            <div className="hidden sm:flex items-center space-x-2 lg:space-x-1.5 ml-4 lg:ml-3">
               {/* Discord Icon */}
               <a
                 href="#"
-                className="w-8 h-8 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
+                className="w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
               >
                 <Image
                   src="/images/discord.png"
                   alt="Discord"
                   width={28}
                   height={28}
-                  className="object-contain"
+                  className="object-contain lg:w-5 lg:h-5"
                 />
               </a>
 
               {/* Telegram Icon */}
               <a
                 href="#"
-                className="w-8 h-8 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
+                className="w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
               >
                 <Image
                   src="/images/telegram.png"
                   alt="Telegram"
                   width={28}
                   height={28}
-                  className="object-contain"
+                  className="object-contain lg:w-5 lg:h-5"
                 />
               </a>
 
               {/* Twitter/X Icon */}
               <a
                 href="#"
-                className="w-8 h-8 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
+                className="w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
               >
                 <Image
                   src="/images/twitter.png"
                   alt="Twitter"
                   width={28}
                   height={28}
-                  className="object-contain"
+                  className="object-contain lg:w-5 lg:h-5"
                 />
               </a>
 
               {/* Info Icon */}
               <div
                 onClick={() => setIsInfoModalOpen(true)}
-                className="w-8 h-8 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
+                className="w-8 h-8 lg:w-6 lg:h-6 flex items-center justify-center cursor-pointer hover:opacity-80 transition-opacity"
               >
                 <Image
                   src="/images/info.png"
                   alt="Info"
                   width={28}
                   height={28}
-                  className="object-contain"
+                  className="object-contain lg:w-5 lg:h-5"
                 />
               </div>
             </div>
           </div>
 
           {/* Buttons Section (using the most compact version) */}
-          <div className="flex items-center space-x-1.5 sm:space-x-2 md:space-x-3 mb-2">
+          <div className="flex items-center space-x-1.5 sm:space-x-2 md:space-x-3 lg:space-x-2 mb-2 lg:mb-1">
             {/* Create Token Button */}
             <button
               className={`${styles.headerBtn}`}
