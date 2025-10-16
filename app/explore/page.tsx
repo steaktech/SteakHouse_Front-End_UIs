1618c58420ef4fc8dce7cfddbd5df9f4a7b8adb7
dev-vishwa <isuru.vishwanath55@gmail.com>
2025-10-16 18:18:27 +0530

    explore page

---
+++ app/explore/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import { ExplorerWidget } from "@/app/components/Widgets/ExplorerWidget";
+
+export default function ExplorePage() {
+  return <ExplorerWidget isOpen={true} onClose={() => {}} />;
+}
