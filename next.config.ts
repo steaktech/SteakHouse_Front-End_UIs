2f1ba684e9150d6eeb55b59461af6028b0343c19
dev-vishwa <isuru.vishwanath55@gmail.com>
2025-10-20 11:53:46 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -15,13 +15,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      { source: '/locker', destination: 'https://locker.steakhouse.finance' },
-      { source: '/explore', destination: 'https://explore.steakhouse.finance' },
-      { source: '/trading-chart/:token', destination: 'https://curve.steakhouse.finance/:token' },
-    ];
-  },
 };
 
 export default nextConfig;
